a22c42270673971d313b492342795db720dd7d89
Magnus Meidahl <Magnus.meidahl@gmail.com>
2021-12-10 16:05:25 +0100

    updateretigen

--- src/Components/page1.js
+++ src/Components/page1.js
@@ -3,7 +3,14 @@ import './page1.css';
 export default function Page1() {
     return (
         <div class="container2">
-            <div class="item a">A</div>
+            <div class="item a">
+                <p align="center">
+                    <img src="https://github.com/MagnusMeidahl/gyldne-snit/blob/master/Images/Fck.jpg?raw=true"></img>
+                    <br />
+                    Fck trøje til salg
+                </p>
+
+            </div>
             <div class="item b">B</div>
             <div class="item c">C</div>
             <div class="item d">D</div>
