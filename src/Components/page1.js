db4661660a5c84b6012ba54cdb9adb740ab61f7f
Magnus Meidahl <Magnus.meidahl@gmail.com>
2021-12-10 17:05:33 +0100

    First take on the golden ratio

--- src/Components/page1.js
+++ src/Components/page1.js
@@ -4,19 +4,34 @@ export default function Page1() {
     return (
         <div class="container2">
             <div class="item a">
-                <p align="center">
-                    <img src="https://github.com/MagnusMeidahl/gyldne-snit/blob/master/Images/Fck.jpg?raw=true"></img>
-                    <br />
-                    Fck trøje til salg
-                </p>
+                <div class="container">
+                    <div class="text-block">
+                        <h4>Corona I Danmark</h4>
+                    </div>
+                    <img style={{ width: 800 }} src="https://www.rksk.dk/Admin/Public/GetImage.ashx?Image=/Files/Images/Nyheder/1_September2020/roedtdk-kortpixled.jpg&width=650&height=370" alt="Corona I Danmark"></img>
+                </div>
+            </div>
+            <div class="item b">
+
+            <div class="container">
+                <h5>5000 smittede.</h5>
+                <img style={{ width: 400 }} src="https://media.lex.dk/media/56011/standard_SARS-CoV-2_without_background.png" alt="FCK Trøje"></img>
+            </div>
+
 
             </div>
-            <div class="item b">B</div>
-            <div class="item c">C</div>
-            <div class="item d">D</div>
-            <div class="item e">E</div>
-            <div class="item f">F</div>
-            <div class="item g">G</div>
+            <div class="item c">
+                <div class="container">
+                    <h5>Køb billig Iphone kun 12000 kr.</h5>
+                    <img style={{ width: 400 }} src="https://cdn.pixabay.com/photo/2020/11/18/13/51/iphone-12-5755365_960_720.jpg" alt="FCK Trøje"></img>
+                </div>
+            </div>
+            <div class="item d">
+                <img src="https://openclipart.org/image/800px/299282"></img>
+            </div>
+            <div class="item e">
+                <a href="https://www.w3schools.com">Vigtig Corona nyhed</a>
+            </div>
         </div>
     )
 }
