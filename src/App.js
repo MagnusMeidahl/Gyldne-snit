b345c00d7384160be5f05ba9eb59077fdad557a9
Magnus Meidahl <Magnus.meidahl@gmail.com>
2021-12-10 15:28:14 +0100

    Updateret

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import Page1 from "./Components/page1"
+import Page2 from "./Components/page2"
+import Navigation from "./Components/Navigation";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Navigation />
+        <br />
+        <Routes>
+          <Route exact path="page1" element={<Page1 />} />
+          <Route exact path="page2" element={<Page2 />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+
+// https://dev.to/salehmubashar/react-router-dom-36a2
---
+++ src/Components/Navigation.js
@@ -0,0 +1,23 @@
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+    return (
+        <>
+        <Navbar collapseOnSelect expand='sm' bg='dark' variant='dark'>
+            <Container>
+                <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                <Navbar.Collapse id='responsive-navbar-nav'>
+                    <Nav>
+                        <Nav.Link as={Link} to='page1'>Page 1</Nav.Link>
+                        <Nav.Link as={Link} to='page2'>Page 2</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        </>
+    );
+}
+
+export default Navigation
+// https://medium.com/swlh/responsive-navbar-using-react-bootstrap-5e0e0bd33bd6
